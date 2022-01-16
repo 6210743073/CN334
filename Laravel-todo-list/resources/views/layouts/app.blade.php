a1281b2d961a1d59939d55541a832c827081e394
Eakkasitt <48272428+Zephy1337@users.noreply.github.com>
2022-01-17 01:40:16 +0700

    Change background image

--- Laravel-todo-list/resources/views/layouts/app.blade.php
+++ Laravel-todo-list/resources/views/layouts/app.blade.php
@@ -21,7 +21,7 @@
     <body class="font-sans antialiased">
         <x-jet-banner />
 
-        <div class="min-h-screen bg-gray-100">
+        <div class="min-h-screen bg-gray-100" style="background-image: url('https://i.pinimg.com/originals/d8/99/05/d89905fd18296f65fada6ef52458ca8c.gif'); background-size: 100% 100%;">
             @livewire('navigation-menu')
 
             <!-- Page Heading -->
