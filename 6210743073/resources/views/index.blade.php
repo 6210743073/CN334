67334524704e013e6e7e2c767fbd6044cd4c1ffa
6210743073 <poohmiphan@outlook.com>
2022-02-16 18:36:40 +0700

    'updateport'

---
+++ 6210743073/resources/views/index.blade.php
@@ -0,0 +1,19 @@
+@extends('layouts.app')
+@section('content')
+    <div class="background-image grid grid-cols-1 m-auto">
+        <div class="flex text-gray-100 pt-10">
+            <div class="m-auto pt-4 pb-16 sm:m-auto w-4/5 block text-center">
+                <h1 class="sm:text-white text-5xl uppercase font-bold text-shadow-md pb-14">โดยที่การไม่นำพาและการหมิ่นในคุณค่าของสิทธิมนุษยชน</h1>
+                <a href="/blog" class="text-center py-2 px-4 rounded-full bg-gray-50 text-gray-700 font-bold text-xl uppercase">อ่านต่อ</a>
+            </div>
+        </div>
+    </div>
+    <div class="sm:grid grid-cols-2 gap-20 w-25 mx-auto py-15 border-b border-gray-200 over">
+        <div>
+            <img src="https://images.unsplash.com/photo-1601342630314-8427c38bf5e6?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=391&q=80" alt="" width="700"/>
+        </div>
+        <div>
+
+        </div>
+    </div>
+@endsection 
