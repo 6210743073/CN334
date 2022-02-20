770273f1b9e1e737248a1b66b7aaf7629609709f
pwat16918 <pwat999@hotmail.com>
2022-02-20 14:23:37 +0700

    Create Backend Auth page

--- 6210742315/MyPortfolio/routes/web.php
+++ 6210742315/MyPortfolio/routes/web.php
@@ -19,4 +19,7 @@ Route::get('/', function () {
 Route::get('/login', function () {
     return view('auth/login');
 });
+Route::get('/register', function () {
+    return view('auth/register');
+});
 //Auth::routes();
